9c5f86b02d3b2980426ca172a392fc689ee57135
Remtori <lqv.remtori@gmail.com>
2020-07-10 23:09:28 +0700

    Update tutorial setup

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -1,9 +1,42 @@
 import { h } from 'preact';
-import { useTutorial } from '../hooks/useTutorial';
-import { Scene } from './routes';
+import { useEffect, useState } from 'preact/hooks';
 import { route } from 'preact-router';
+import { Events } from 'matter-js';
+
+import { engine }  from '../game/PhysicEngine';
+import { getControlledTank } from '../game/TankController';
+import { Scene } from './routes';
+import { tutorials } from '../tutorials';
 
 const skipTutorial = () => route(Scene.InGame);
+
+function useTutorial() {
+
+	const [ tutorialState, nextTutorialState ] = useState(0);
+
+	useEffect(() => {
+		function onUpdate() {
+			const tank = getControlledTank();
+			if (!tank) return;
+
+			if (tutorials[tutorialState].isComplete(tank)) {
+
+				if (tutorialState + 1 == tutorials.length)
+					skipTutorial();
+
+				nextTutorialState(tutorialState + 1);
+			}
+		}
+
+		Events.on(engine, 'afterUpdate', onUpdate);
+
+		return () => Events.off(engine, 'afterUpdate', onUpdate);
+	}, [ tutorialState ]);
+
+	return {
+		text: tutorials[tutorialState].text,
+	}
+}
 
 export function Tutorial() {
 
--- src/tutorials.ts
+++ src/tutorials.ts
@@ -1,14 +1,19 @@
 import { Tank } from './game/Tank';
 
-const epsilon = 5;
+const epsilon = 25;
 const isAround = (a: number, b: number) => Math.abs(a - b) < epsilon;
 
 export const tutorials = [
-	// 0
 	{
-		text: `Try using W, A, S, D to move around`,
+		text: `Try using W, A, S, D to move to the X mark`,
 		isComplete: (tank: Tank) => {
-			return isAround(tank.body.position.x, 200) &&  isAround(tank.body.position.y, 200);
+			return isAround(tank.body.position.x, 200) &&  isAround(tank.body.position.y, 100);
 		}
 	},
+	{
+		text: `Try to Mouse Over the other tank and Left-Click to destroy it`,
+		isComplete: (tank: Tank) => {
+			return false;
+		}
+	}
 ];
